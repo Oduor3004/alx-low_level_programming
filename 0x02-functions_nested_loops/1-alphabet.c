45e7693d772fab50a4375f0dbd48509bf9505231
Oduor3004 <oduordennis3757@gmail.com>
2023-08-15 07:55:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char low; 
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	_putchar(low);
+	}
+	_putchar('\n');
+}
